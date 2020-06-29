873d1d5447f839e2e00a5bfe5266a21c7e62040c
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-06-28 23:14:40 -0400

    Rm queue (#29)
    * removed concurrentqueue, created utility header
    
    * use copies to prevent branching in postPen hot loop
    
    * removed concurrentqueue dependency - no longer needed

--- test.cpp
+++ test.cpp
@@ -12,17 +12,17 @@ void runtime() {
     unsigned int runs = 1;
     for (int i = 0; i < runs; i++) {
 
-        test = new shell::shell(.460, 780, .292, 1460, 2574, 6, .001, 2, 91, 60,
-                                76, "Yamato");
-        // test = new shell::shell(.460, 780, .292, 1460, 2574, 6, .033, 76, 45,
-        //                        60, 0, "Yamato");
+        //test = new shell::shell(.460, 780, .292, 1460, 2574, 6, .001, 2, 91, 60,
+        //                        76, "Yamato");
+        test = new shell::shell(.460, 780, .292, 1460, 2574, 6, .033, 76, 45,
+                                60, 0, "Yamato");
 
 
         // test = new shell::shell(.102, 805, .3536, 15.2, 2300, 10, .01, 17,
         // 45,
         //                        60, "Yamato");
         auto t1 = std::chrono::high_resolution_clock::now();
-        sc.calculateImpact<shell::numerical::forwardEuler, false>(*test, true);
+        sc.calculateImpact<shell::numerical::adamsBashforth5, false>(*test, true);
         auto t2 = std::chrono::high_resolution_clock::now();
         total += (double)std::chrono::duration_cast<std::chrono::nanoseconds>(
                      t2 - t1)
@@ -43,13 +43,13 @@ void runtime() {
 
     sc.calculateAngles(70, 0, *test);
 
-    test->printImpactData();
+    //test->printImpactData();
     // std::cout << test->interpolateDistanceImpact(
     //                 30000, shell::impact::impactDataIndex::rawPen)
     //          << "\n";
     // test->printTrajectory(0);
-    // test->printPostPenData();
-    test->printAngleData();
+    test->printPostPenData();
+    //test->printAngleData();
 
     std::cout << std::fixed << std::setprecision(10)
               << total / runs / 1000000000 << std::endl;
