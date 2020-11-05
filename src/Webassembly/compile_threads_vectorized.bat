8e8797230e2305c0986750da77dffef1cc63d72a
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-11-05 13:02:19 -0500

    fix: use dlmalloc instead of emmalloc for threading compilations

--- src/Webassembly/compile_threads_vectorized.bat
+++ src/Webassembly/compile_threads_vectorized.bat
@@ -1,4 +1,4 @@
 rem experimental multithreaded and vectorized version
 emcc --bind -o shellWasm.wasm.js shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasmTV"^
- -s ENVIRONMENT="web,worker" -s MALLOC=emmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=1 --closure 1 -flto^
- -s PTHREAD_POOL_SIZE=4 -msimd128 -v
+ -s ENVIRONMENT="web,worker" -s MALLOC=dlmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=1 --closure 1 -flto^
+ -msimd128 -v
