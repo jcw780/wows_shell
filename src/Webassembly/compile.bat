94f847fb45376eadab8c7f7830893607058e4e38
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-11-06 18:06:31 -0500

    fix: shellCalc characteristics setters not working in js [note: reproduce and file a bug report?]; std::move preventing copy elision while passing postPen angles; added warnings to compile commands

--- src/Webassembly/compile.bat
+++ src/Webassembly/compile.bat
@@ -1,3 +1,4 @@
 rem unvectorized/unthreaded version
 emcc --bind -o shellWasm.wasm.js shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasm"^
- -s ENVIRONMENT="web" -s MALLOC=emmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=1 --closure 1 -flto -v
+ -s ENVIRONMENT="web" -s MALLOC=emmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=1 --closure 1 -flto -v^
+ -Wall -Wextra -Wpedantic
