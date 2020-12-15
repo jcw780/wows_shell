dcc00b1ef8a86fe3ebf63a97fa671e130ade4889
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-12-14 23:42:07 -0500

    fix: reenabled multifile wasm

--- src/Webassembly/compile_threads_vectorized.bat
+++ src/Webassembly/compile_threads_vectorized.bat
@@ -1,4 +1,4 @@
 rem experimental multithreaded and vectorized version
-emcc --bind -o shellWasmTV.wasm.js shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasmTV"^
- -s ENVIRONMENT="web,worker" -s MALLOC=dlmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=1 --closure 1 -flto^
+emcc --bind -o build/shellWasmTV.js shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasmTV"^
+ -s ENVIRONMENT="web,worker" -s MALLOC=dlmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=0 --closure 1 -flto^
  -msimd128 -v -Wall -Wextra -Wpedantic -pthread
