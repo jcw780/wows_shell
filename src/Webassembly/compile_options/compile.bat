1728df0ca9ee9258b77f54e952cd9015dc51563f
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-12-18 20:04:41 -0500

    refactor: organized wasm compile options into its own folder

--- src/Webassembly/compile_options/compile.bat
+++ src/Webassembly/compile_options/compile.bat
@@ -1,4 +1,4 @@
 rem unvectorized/unthreaded version
-emcc --bind -o build/shellWasm.js shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasm"^
+emcc --bind -o ../build/shellWasm.js ../shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasm"^
  -s ENVIRONMENT="web" -s MALLOC=emmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=0 --closure 1 -flto -v^
  -Wall -Wextra -Wpedantic
--- src/Webassembly/compile_options/compile_threads.bat
+++ src/Webassembly/compile_options/compile_threads.bat
@@ -1,4 +1,4 @@
 rem experimental multithreaded version
-emcc --bind -o build/shellWasmT.js shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasmT"^
+emcc --bind -o ../build/shellWasmT.js ../shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasmT"^
  -s ENVIRONMENT="web,worker" -s MALLOC=dlmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=0 --closure 1 -flto^
  -v -Wall -Wextra -Wpedantic -pthread
