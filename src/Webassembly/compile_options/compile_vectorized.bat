665079c2f2b774a69eedd494f0f1345e29e14e7c
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-12-23 14:39:32 -0500

    fix: added -s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0 to compile options

--- src/Webassembly/compile_options/compile_vectorized.bat
+++ src/Webassembly/compile_options/compile_vectorized.bat
@@ -1,4 +1,4 @@
 rem vectorized/unthreaded version
 emcc --bind -o ../build/shellWasmV.js ../shellWasm.cpp --std=c++17 -O3 -s ASSERTIONS=1 -s MODULARIZE=1 -s EXPORT_NAME="ShellWasmV"^
  -s ENVIRONMENT="web" -s MALLOC=emmalloc -s ALLOW_MEMORY_GROWTH=1 -s FILESYSTEM=0 -s SINGLE_FILE=0 --closure 1 -flto -msimd128 -v^
- -Wall -Wextra -Wpedantic
+ -Wall -Wextra -Wpedantic -s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0
