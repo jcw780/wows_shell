54b51e00e4b3311b99c547c52e11f871e750f657
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-12-26 20:58:53 -0500

    refactor: deprecated shellCombined in Python and Webassembly extensions

--- src/Webassembly/shellWasm.cpp
+++ src/Webassembly/shellWasm.cpp
@@ -199,8 +199,6 @@ emscripten::val getImpactSizedPointArrayFuseStatus(shell &s,
 
 }  // namespace pointArray
 
-#define ENABLE_SPLIT_SHELL
-#ifdef ENABLE_SPLIT_SHELL
 class shellWasm {
    public:
     shell s;
@@ -404,201 +402,6 @@ class shellCalcWasm : public shellCalc {
                          fast);
     }
 };
-#endif
-
-#define ENABLE_SHELL_COMBINED
-#ifdef ENABLE_SHELL_COMBINED
-class shellCombined {
-   private:
-    shellCalc calc;
-    // shell s;
-    std::vector<shell> ships;
-
-   public:
-    /*shellCombined(const double caliber, const double v0, const double cD,
-                  const double mass, const double krupp,
-                  const double normalization, const double fuseTime,
-                  const double threshold, const double ricochet0,
-                  const double ricochet1) {
-        s.setValues(caliber, v0, cD, mass, krupp, normalization, fuseTime,
-                    threshold, ricochet0, ricochet1, "ship");
-    }*/
-    shellCombined(const unsigned int numShips = 1) { resize(numShips); }
-    void resize(const unsigned int numShips) { ships.resize(numShips); }
-
-    void setValues(const double caliber, const double v0, const double cD,
-                   const double mass, const double krupp,
-                   const double normalization, const double fuseTime,
-                   const double threshold, const double ricochet0,
-                   const double ricochet1, const double nonAP,
-                   const int shipIndex) {
-        ships[shipIndex].setValues(caliber, v0, cD, mass, krupp, normalization,
-                                   fuseTime, threshold, ricochet0, ricochet1,
-                                   nonAP, "");
-    }
-
-    void setMax(const double max) { calc.set_max(max); }
-    void setMin(const double min) { calc.set_min(min); }
-    void setPrecision(const double precision) { calc.set_precision(precision); }
-    void setX0(const double x0) { calc.set_x0(x0); }
-    void setY0(const double y0) { calc.set_y0(y0); }
-    void setDtMin(const double dt) { calc.set_dt_min(dt); }
-    void setXf0(const double xf0) { calc.set_xf0(xf0); }
-    void setYf0(const double yf0) { calc.set_yf0(yf0); }
-    void setDtf(const double dtf) { calc.set_dtf(dtf); }
-
-    // Impact Wrappers
-    // Default: Adams Bashforth 5
-
-    template <numerical Numerical>
-    void calcImpact() {
-        for (auto &s : ships) {
-#ifdef __EMSCRIPTEN_PTHREADS__
-            calc.calculateImpact<Numerical, false>(s, false);
-#else
-            calc.calculateImpact<Numerical, false>(s, false, 1);
-#endif
-        }
-    }
-
-    // Sizes are the same for both ships
-    int impactSize() { return ships[0].impactSize; }
-    int impactSizeAligned() { return ships[0].impactSizeAligned; }
-
-    std::vector<double> impactData(const std::size_t shipIndex) {
-        if (ships[shipIndex].completedImpact) {
-            return ships[shipIndex].impactData;
-        } else {
-            throw std::runtime_error("Impact data not generated");
-        }
-    }
-
-    double getImpactPoint(const std::size_t shipIndex, const std::size_t i,
-                          const std::size_t j) {
-        // NOT SAFE - PLEASE MAKE SURE YOU ARE NOT OVERFLOWING
-        return ships[shipIndex].get_impact(i, j);
-    }
-
-    // These functions are for producing arrays suitable for chart.js scatter
-    // plots
-    emscripten::val getImpactPointArray(const std::size_t shipIndex,
-                                        const std::size_t xIndex,
-                                        const std::size_t yIndex) {
-        return pointArray::getImpactPointArray(ships[shipIndex], xIndex,
-                                               yIndex);
-    }
-
-    // Angle Data Wrappers
-
-    void calcAngles(const double thickness, const double inclination) {
-        for (auto &s : ships) {
-#ifdef __EMSCRIPTEN_PTHREADS__
-            calc.calculateAngles(thickness, inclination, s);
-#else
-            calc.calculateAngles(thickness, inclination, s, 1);
-#endif
-        }
-    }
-
-    std::vector<double> angleData(const std::size_t shipIndex) {
-        if (ships[shipIndex].completedAngles) {
-            return ships[shipIndex].angleData;
-        } else {
-            throw std::runtime_error("Angle data not generated");
-        }
-    }
-
-    double getAnglePoint(const std::size_t shipIndex, const std::size_t row,
-                         const std::size_t impact) {
-        // NOT SAFE - PLEASE MAKE SURE YOU ARE NOT OVERFLOWING
-        return ships[shipIndex].get_angle(row, impact);
-    }
-
-    emscripten::val getAnglePointArray(const std::size_t shipIndex,
-                                       const std::size_t xIndex,
-                                       const std::size_t yIndex) {
-        return pointArray::getAnglePointArray(ships[shipIndex], xIndex, yIndex);
-    }
-
-    // Post Penetration Wrappers
-
-    void calcPostPen(const double thickness, const double inclination,
-                     emscripten::val v, const bool changeDirection,
-                     const bool fast) {
-        std::vector<double> input =
-            emscripten::convertJSArrayToNumberVector<double>(v);
-        for (auto &s : ships) {
-#ifdef __EMSCRIPTEN_PTHREADS__
-            calc.calculatePostPen(thickness, inclination, s, input,
-                                  changeDirection, fast);
-#else
-            calc.calculatePostPen(thickness, inclination, s, input,
-                                  changeDirection, fast, 1);
-#endif
-        }
-    }
-
-    // Sizes are the same for both ships
-    int postPenSize() { return ships[0].postPenSize; }
-
-    std::vector<double> postPenData(const std::size_t shipIndex) {
-        if (ships[shipIndex].completedPostPen) {
-            return ships[shipIndex].postPenData;
-        } else {
-            throw std::runtime_error("Impact data not generated");
-        }
-    }
-
-    double getPostPenPoint(const std::size_t shipIndex, const std::size_t i,
-                           const std::size_t j, const std::size_t k) {
-        // NOT SAFE - PLEASE MAKE SURE YOU ARE NOT OVERFLOWING
-        return ships[shipIndex].get_postPen(i, j, k);
-    }
-
-    emscripten::val getPostPenPointArray(const std::size_t shipIndex,
-                                         const std::size_t angle,
-                                         const std::size_t xIndex,
-                                         const std::size_t yIndex) {
-        return pointArray::getPostPenPointArray(ships[shipIndex], angle, xIndex,
-                                                yIndex);
-    }
-
-    emscripten::val getPostPenPointArrayFuseStatus(const std::size_t shipIndex,
-                                                   const bool addCondition,
-                                                   const std::size_t angle,
-                                                   const std::size_t xIndex,
-                                                   const std::size_t yIndex) {
-        return pointArray::getPostPenPointArrayFuseStatus(
-            ships[shipIndex], addCondition, angle, xIndex, yIndex);
-    }
-
-    // Print Functions
-
-    void printImpact(const std::size_t shipIndex) {
-        if (ships[shipIndex].completedImpact) {
-            ships[shipIndex].printImpactData();
-        } else {
-            throw std::runtime_error("Impact data not generated");
-        }
-    }
-
-    void printAngles(const std::size_t shipIndex) {
-        if (ships[shipIndex].completedAngles) {
-            ships[shipIndex].printAngleData();
-        } else {
-            throw std::runtime_error("Angle data not generated");
-        }
-    }
-
-    void printPostPen(const std::size_t shipIndex) {
-        if (ships[shipIndex].completedPostPen) {
-            ships[shipIndex].printPostPenData();
-        } else {
-            throw std::runtime_error("PostPen data not generated");
-        }
-    }
-};
-#endif
 
 template <typename Input, typename Keys, typename Output, typename KeyGenerator>
 void extractDictToArray(Input &input, Keys &keys, Output &output,
@@ -730,7 +533,6 @@ EMSCRIPTEN_BINDINGS(shellWasm) {
         .property("maxDistance", &dispersionParams::maxDistance)
         .property("sigma", &dispersionParams::sigma);
 
-#ifdef ENABLE_SPLIT_SHELL
     emscripten::class_<shellWasm>("shell")
         .constructor<double, double, double, double, double, double, double,
                      double, double, double, double, std::string>()
@@ -800,56 +602,6 @@ EMSCRIPTEN_BINDINGS(shellWasm) {
         .function("calcAngles", &shellCalcWasm::calcAngles)
         .function("calcDispersion", &shellCalcWasm::calcDispersion)
         .function("calcPostPen", &shellCalcWasm::calcPostPen);
-#endif
-#ifdef ENABLE_SHELL_COMBINED
-    emscripten::class_<shellCombined>("shellCombined")
-        .constructor<int>()
-        .function("resize", &shellCombined::resize)
-        .function("setValues", &shellCombined::setValues)
-        .function("setMax", &shellCombined::setMax)
-        .function("setMin", &shellCombined::setMin)
-        .function("setPrecision", &shellCombined::setPrecision)
-        .function("setX0", &shellCombined::setX0)
-        .function("setY0", &shellCombined::setY0)
-        .function("setDtMin", &shellCombined::setDtMin)
-        .function("setXf0", &shellCombined::setXf0)
-        .function("setYf0", &shellCombined::setYf0)
-        .function("setDtf", &shellCombined::setDtf)
-
-        .function("calcImpact",
-                  &shellCombined::calcImpact<numerical::adamsBashforth5>)
-        .function("calcImpactAdamsBashforth5",
-                  &shellCombined::calcImpact<numerical::adamsBashforth5>)
-        .function("calcImpactForwardEuler",
-                  &shellCombined::calcImpact<numerical::forwardEuler>)
-        .function("calcImpactRungeKutta2",
-                  &shellCombined::calcImpact<numerical::rungeKutta2>)
-        .function("calcImpactRungeKutta4",
-                  &shellCombined::calcImpact<numerical::rungeKutta4>)
-
-        .function("getImpactPoint", &shellCombined::getImpactPoint)
-        .function("impactData", &shellCombined::impactData)
-        .function("getImpactPointArray", &shellCombined::getImpactPointArray)
-        .function("getImpactSize", &shellCombined::impactSize)
-        .function("getImpactSizeAligned", &shellCombined::impactSizeAligned)
-
-        .function("calcAngles", &shellCombined::calcAngles)
-        .function("angleData", &shellCombined::angleData)
-        .function("getAnglePoint", &shellCombined::getAnglePoint)
-        .function("getAnglePointArray", &shellCombined::getAnglePointArray)
-
-        .function("calcPostPen", &shellCombined::calcPostPen)
-        .function("getPostPenPoint", &shellCombined::getPostPenPoint)
-        .function("postPenData", &shellCombined::postPenData)
-        .function("getPostPenPointArray", &shellCombined::getPostPenPointArray)
-        .function("getPostPenPointArrayFuseStatus",
-                  &shellCombined::getPostPenPointArrayFuseStatus)
-        .function("getPostPenSize", &shellCombined::postPenSize)
-
-        .function("printImpact", &shellCombined::printImpact)
-        .function("printAngles", &shellCombined::printAngles)
-        .function("printPostPen", &shellCombined::printPostPen);
-#endif
     emscripten::register_vector<double>("vector<double>");
 
     // Enums
