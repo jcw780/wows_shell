c55982350bbad5b0488754cac8bdc5019f91174f
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-12-27 23:04:27 -0500

    fix: using setuptools for setup.py

--- src/Python/setup.py
+++ src/Python/setup.py
@@ -5,11 +5,16 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 ext_modules = [
     Pybind11Extension(
         "wows_shell",
-        sorted(glob("*.cpp")),
+        ["wows_shell_python.cpp"],
+        cxx_std="c++17"
     ),
 ]
 
 setup(
+    name="wows_shell",
+    version="1.1.2",
+    description="Python extension of wows_shell",
+    url="https://github.com/jcw780/wows_shell",
     cmdclass={"build_ext": build_ext},
     ext_modules=ext_modules
 )
